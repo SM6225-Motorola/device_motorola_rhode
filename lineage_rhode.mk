79211078a55830f2f27d019e54856e6627a4fc6d
ZetLink <royalezetlink@gmail.com>
2024-12-01 17:09:35 +0100

    Init risingOS

--- lineage_rhode.mk
+++ lineage_rhode.mk
@@ -10,7 +10,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from device
 $(call inherit-product, device/motorola/rhode/device.mk)
 
-# Inherit some common LineageOS stuff.
+# Inherit some common RisingOS stuff.
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 # Device identifier. This must come after all inclusions.
@@ -20,9 +20,18 @@ PRODUCT_BRAND := motorola
 PRODUCT_MODEL := moto g52
 PRODUCT_MANUFACTURER := motorola
 
+# RisingFlags
+TARGET_ENABLE_BLUR := false
+RISING_BATTERY := 5000mah
+TARGET_BOOT_ANIMATION_RES := 1080
+TARGET_SUPPORTS_QUICK_TAP := true
+TARGET_PREBUILT_LAWNCHAIR_LAUNCHER := true
+
 PRODUCT_GMS_CLIENTID_BASE := android-motorola
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
+    RisingChipset="Snapdragon 680" \
+    RisingMaintainer="ZetLink" \
     BuildDesc="rhode_g-user 13 T2SRS33.72-22-4-8 07e10-4fa4c release-keys" \
     BuildFingerprint=motorola/rhode_g/rhode:13/T2SRS33.72-22-4-8/07e10-4fa4c:user/release-keys \
     DeviceProduct=rhode_g
